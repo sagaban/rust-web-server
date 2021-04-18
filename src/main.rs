8b8a9dce4eae14f1138d745bcf50b047b4168d78
Santiago Bandiera <sagaban@hotmail.com>
2021-04-18 12:44:45 -0300

    Add basic web server that responds a static file

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use actix_files::{NamedFile};
+use actix_web::{web, App, HttpServer, Result};
+
+async fn index() -> Result<NamedFile> {
+    Ok(NamedFile::open("./static/index.html")?)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Listening on port 8080");
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
