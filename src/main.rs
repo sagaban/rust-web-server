0f1da67d0c30084115fc89c9d84e2beeb527b937
Santiago Bandiera <sagaban@hotmail.com>
2021-04-18 16:38:52 -0300

    Render using dynamic templates

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,41 @@
-use actix_files::{Files, NamedFile};
-use actix_web::{web, App, HttpServer, Result};
+use actix_files::Files;
+use actix_web::{web, App, HttpResponse, HttpServer};
+use handlebars::Handlebars;
+use serde_json::json;
 
-async fn index() -> Result<NamedFile> {
-    Ok(NamedFile::open("./static/index.html")?)
+async fn index(hb: web::Data<Handlebars<'_>>) -> HttpResponse {
+    let data = json!({
+        "project_name": "Catdex",
+        "cats": [
+            {
+                "name": "British short hair",
+                "image_path": "/static/image/british-short-hair.jpg"
+            },
+            {
+                "name": "Persian",
+                "image_path": "/static/image/persian.jpg"
+            },
+            {
+                "name": "Ragdoll",
+                "image_path": "/static/image/ragdoll.jpg"
+            }
+        ]
+    });
+    let body = hb.render("index", &data).unwrap();
+    HttpResponse::Ok().body(body)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let mut handlebars = Handlebars::new();
+    handlebars
+        .register_templates_directory(".html", "./static/")
+        .unwrap();
+    let handlebars_ref = web::Data::new(handlebars);
     println!("Listening on port 8080");
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
+            .app_data(handlebars_ref.clone())
             .service(
                 Files::new("/static", "static")
                     .show_files_listing(),
